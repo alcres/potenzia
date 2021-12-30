26f369c9b15eac9d1390828cacda37f255b9ff56
Alvaro Criado <acriado.it@hbplat.es>
2021-12-30 16:42:47 +0100

    add features

--- src/gproPA/LoginIntercept.java
+++ src/gproPA/LoginIntercept.java
@@ -11,6 +11,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.nodes.FormElement;
 import org.jsoup.select.Elements;
 
+import model.Coche;
+import model.Niveles;
+import model.NivelesFin;
 import model.Vuelta;
 
 public class LoginIntercept {
@@ -47,24 +50,201 @@ public class LoginIntercept {
 		// System.out.print(positionResponse.parse().html());
 		Document doc = Jsoup.parse(positionResponse.parse().html());
 
-		Elements masthead = doc.select("table.borderbottom");
 
+
+		Elements coche = doc.select("table.styled.bordered.center");
+		Element c = coche.get(0);
+		Elements trs = c.select("tr");
+		String[][] trtd = new String[trs.size()][];
+		Coche car = new Coche();
+		Niveles nivelInicio = new Niveles();
+		NivelesFin nivelesFin = new NivelesFin();
+		for (int i = 2; i < trs.size(); i++) {
+			Elements tds = trs.get(i).select("td");
+			trtd[i] = new String[tds.size()];
+
+
+
+			for (int j = 0; j < tds.size(); j++) {
+			//	System.out.println(i+": |"+j+": | TEXTO: "+tds.get(j).text());
+				if (j == 0) {
+					switch (i) {
+						case 2:
+							car.setChassis(Integer.parseInt(tds.get(j).text()));
+							break;
+						case 4:
+							nivelInicio.setChassis(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+						case 6:
+							nivelesFin.setChassis(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+					}
+				}
+				if (j == 1) {
+					switch (i) {
+						case 2:
+							car.setMotor(Integer.parseInt(tds.get(j).text()));
+							break;
+						case 4:
+							nivelInicio.setMotor(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+						case 6:
+							nivelesFin.setMotor(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+					}
+				}
+				if (j == 2) {
+					switch (i) {
+						case 2:
+							car.setAleD(Integer.parseInt(tds.get(j).text()));
+							break;
+						case 4:
+							nivelInicio.setAleD(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+						case 6:
+							nivelesFin.setAleD(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+					}
+				}
+				if (j == 3) {
+					switch (i) {
+						case 2:
+							car.setAleT(Integer.parseInt(tds.get(j).text()));
+							break;
+						case 4:
+							nivelInicio.setAleT(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+						case 6:
+							nivelesFin.setAleT(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+					}
+				}
+				if (j == 4) {
+					switch (i) {
+						case 2:
+							car.setFondo(Integer.parseInt(tds.get(j).text()));
+							break;
+						case 4:
+							nivelInicio.setFondo(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+						case 6:
+							nivelesFin.setFondo(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+					}
+				}
+				if (j == 5) {
+					switch (i) {
+						case 2:
+							car.setRefrigeracion(Integer.parseInt(tds.get(j).text()));
+							break;
+						case 4:
+							nivelInicio.setRefrigeracion(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+						case 6:
+							nivelesFin.setRefrigeracion(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+					}
+				}
+				if (j == 6) {
+					switch (i) {
+						case 2:
+							car.setPontones(Integer.parseInt(tds.get(j).text()));
+							break;
+						case 4:
+							nivelInicio.setPontones(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+						case 6:
+							nivelesFin.setPontones(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+					}
+				}
+				if (j == 7) {
+					switch (i) {
+						case 2:
+							car.setCaja(Integer.parseInt(tds.get(j).text()));
+							break;
+						case 4:
+							nivelInicio.setCaja(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+						case 6:
+							nivelesFin.setCaja(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+					}
+				}
+				if (j == 8) {
+					switch (i) {
+						case 2:
+							car.setFrenos(Integer.parseInt(tds.get(j).text()));
+							break;
+						case 4:
+							nivelInicio.setFrenos(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+						case 6:
+							nivelesFin.setFrenos(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+					}
+				}
+				if (j == 9) {
+					switch (i) {
+						case 2:
+							car.setSuspension(Integer.parseInt(tds.get(j).text()));
+							break;
+						case 4:
+							nivelInicio.setSuspension(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+						case 6:
+							nivelesFin.setSuspension(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+					}
+				}
+				if (j == 10) {
+					switch (i) {
+						case 2:
+							car.setElectrico(Integer.parseInt(tds.get(j).text()));
+							break;
+						case 4:
+							nivelInicio.setElectrico(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+						case 6:
+							nivelesFin.setElectrico(Integer.parseInt(tds.get(j).text().substring(0,tds.get(j).text().length()-1)));
+							break;
+					}
+				}
+			}
+		}
 		// System.out.print(masthead.html());
+			Elements masthead = doc.select("table.borderbottom");
 		Element table = masthead.get(0);
-		
+
+
+		List<Vuelta> vueltas = getVueltas(masthead);
+
+		for (Vuelta v : vueltas) {
+			System.out.print(v.getVuelta() + " | ");
+			System.out.print(v.getTiempo() + " | ");
+			System.out.print(v.getTemp() + " | ");
+			System.out.println(v.getClima());
+		}
+	}
+
+	private List<Vuelta> getVueltas(Elements masthead) {
 		//GET RACE LAPS ANALISYS
 		Element laps = masthead.get(2);
 		List<Vuelta> vueltas = new ArrayList<>();
 		Elements trs = laps.select("tr");
 		String[][] trtd = new String[trs.size()][];
-
+		int boost = 0;
 		for (int i = 2; i < trs.size(); i++) {
 			Elements tds = trs.get(i).select("td");
 			trtd[i] = new String[tds.size()];
 			Vuelta v = new Vuelta();
+
 			for (int j = 0; j < tds.size(); j++) {
 				if (j == 0) {
 					v.setVuelta(tds.get(j).text());
+					if(tds.get(j).text().contains("B")){
+						boost++;
+					}
 				}
 				if (j == 1) {
 					v.setTiempo(tds.get(j).text());
@@ -90,13 +270,8 @@ public class LoginIntercept {
 			}
 			vueltas.add(v);
 		}
-
-		for (Vuelta v : vueltas) {
-			System.out.print(v.getVuelta() + " | ");
-			System.out.print(v.getTiempo() + " | ");
-			System.out.print(v.getTemp() + " | ");
-			System.out.println(v.getClima());
-		}
+		System.out.println("Vueltas Boost: "+ boost);
+		return vueltas;
 	}
 
 	public boolean checkElement(String name, Element elem) {
---
+++ src/model/Coche.java
@@ -0,0 +1,106 @@
+package model;
+
+public class Coche {
+
+    private int chassis;
+    private int motor;
+    private int aleD;
+    private int aleT;
+    private int fondo;
+    private int pontones;
+    private int refrigeracion;
+    private int caja;
+    private int frenos;
+    private int suspension;
+    private int electrico;
+
+    public int getChassis() {
+        return chassis;
+    }
+
+    public void setChassis(final int chassis) {
+        this.chassis = chassis;
+    }
+
+    public int getMotor() {
+        return motor;
+    }
+
+    public void setMotor(final int motor) {
+        this.motor = motor;
+    }
+
+    public int getAleD() {
+        return aleD;
+    }
+
+    public void setAleD(final int aleD) {
+        this.aleD = aleD;
+    }
+
+    public int getAleT() {
+        return aleT;
+    }
+
+    public void setAleT(final int aleT) {
+        this.aleT = aleT;
+    }
+
+    public int getFondo() {
+        return fondo;
+    }
+
+    public void setFondo(final int fondo) {
+        this.fondo = fondo;
+    }
+
+    public int getRefrigeracion() {
+        return refrigeracion;
+    }
+
+    public void setRefrigeracion(final int refrigeracion) {
+        this.refrigeracion = refrigeracion;
+    }
+
+    public int getCaja() {
+        return caja;
+    }
+
+    public void setCaja(final int caja) {
+        this.caja = caja;
+    }
+
+    public int getFrenos() {
+        return frenos;
+    }
+
+    public void setFrenos(final int frenos) {
+        this.frenos = frenos;
+    }
+
+    public int getSuspension() {
+        return suspension;
+    }
+
+    public void setSuspension(final int suspension) {
+        this.suspension = suspension;
+    }
+
+    public int getElectrico() {
+        return electrico;
+    }
+
+    public void setElectrico(final int electrico) {
+        this.electrico = electrico;
+    }
+
+
+    public int getPontones() {
+        return pontones;
+    }
+
+    public void setPontones(final int pontones) {
+        this.pontones = pontones;
+    }
+
+}
